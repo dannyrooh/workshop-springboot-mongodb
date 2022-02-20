a97449d9f9f4a28f7547bc64e2e8d5277206502f
Dannyroohm <dannyrooh@hotmail.com>
2022-02-20 17:25:35 -0300

    make project

---
+++ src/main/java/com/webmadria/worrkshopmongodb/WorrkshopmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.webmadria.worrkshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorrkshopmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorrkshopmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webmadria/worrkshopmongodb/WorrkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmadria.worrkshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorrkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
